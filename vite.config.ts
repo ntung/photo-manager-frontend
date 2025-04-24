8708bd33b7ee08560980dc385ae81a43ce9a3d59
Tung Nguyen <nvntung@gmail.com>
2025-04-24 09:46:59 +0100

    Set up https

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as fs from "node:fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, env.VITE_PERM_KEY)),
+        cert: fs.readFileSync(path.resolve(__dirname, env.VITE_PERM_PUB)),
+      },
+      host: '0:0:0:0',
+      watch: {
+        usePolling: true
+      }
     },
-  },
-  plugins: [react({
-    include: "**/*.tsx"
-  })]
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+    plugins: [react({
+      include: "**/*.tsx"
+    })]
+  }
 });
